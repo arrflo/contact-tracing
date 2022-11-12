a329be1e93a55a264035fec95d0964f1a93803e4
romalynfloranda <romalynfloranda0203@gmail.com>
2022-11-12 16:00:15 +0800

    i still don't get option 2

--- prog1.py
+++ prog1.py
@@ -19,6 +19,8 @@ while True:
         "Email" : "  ",
         "Phone Number" : "  "
     }
+
+    
     #if option 1 is chosen
     if user__ == 1:
         dictionary["Fullname"] = input("Fullname: ")
@@ -30,41 +32,44 @@ while True:
         user__ = int(input ("What would you like to do? "))
         #user proceeds to option 2
         if user__ == 2:
-            user1__ = int(input ("What input would you like to search?(type 'all' for all data) "))
-            if user__ == "fullname" or "Fullname":
-                print ("Fullname: ",dictionary["Fullname"])
-            elif user__ == "age" or "Age":
-                print ("Age: ",dictionary["Age"])
-            elif user__ == "address" or "Address":
-                print ("Address: ",dictionary["Address"])
-            elif user__ == "email" or "Email":
-                print ("Email: ",dictionary["Email"])
-            elif user__ == "phone number" or "Phone Number" or "Phone number":
-                print ("Phone Number: ",dictionary["Phone Number"])
-            elif user__ == "all":
+            user1__ = input ("What input would you like to search?(type 'all' for all data) ")
+            if user1__ == "fullname" or "Fullname":
+                print ("Fullname: ", dictionary.get("Fullname"))
+            elif user1__ == "age" or "Age":
+                print ("Age: ", dictionary.get("Age"))
+            elif user1__ == "address" or "Address":
+                print ("Address: ", dictionary.get("Address"))
+            elif user1__ == "email" or "Email":
+                print ("Email: ", dictionary.get("Email"))
+            elif user1__ == "phone number" or "Phone Number" or "Phone number":
+                print ("Phone Number: ", dictionary.get("Phone Number"))
+            elif user1__ == "all":
                 for key, value in dictionary.items():
                     print (key, ":", value)
-        #clears the list
         if user__ == 3:
-            break
-    if user__ == 2:
-        if user__ == 2:
-            user1__ = int(input ("What input would you like to search?(type 'all' for all data) "))
-            if user__ == "fullname" or "Fullname":
-                print ("Fullname: ",dictionary["Fullname"])
-            elif user__ == "age" or "Age":
-                print ("Age: ",dictionary["Age"])
-            elif user__ == "address" or "Address":
-                print ("Address: ",dictionary["Address"])
-            elif user__ == "email" or "Email":
-                print ("Email: ",dictionary["Email"])
-            elif user__ == "phone number" or "Phone Number" or "Phone number":
-                print ("Phone Number: ",dictionary["Phone Number"])
-            elif user__ == "all":
-                for key, value in dictionary.items():
-                    print (key, ":", value)
-    if user__ == 3
-        break
+            a = input("are you sure? (y/n)")
+            if a == "y":
+                break
+    # if user__ == 2:
+        # if user__ == 2:
+        #     user1__ = input ("What input would you like to search?(type 'all' for all data) ")
+        #     if user1__ == "fullname" or "Fullname":
+        #         print ("Fullname: ",dictionary["Fullname"])
+        #     elif user1__ == "age" or "Age":
+        #         print ("Age: ",dictionary["Age"])
+        #     elif user1__ == "address" or "Address":
+        #         print ("Address: ",dictionary["Address"])
+        #     elif user1__ == "email" or "Email":
+        #         print ("Email: ",dictionary["Email"])
+        #     elif user1__ == "phone number" or "Phone Number" or "Phone number":
+        #         print ("Phone Number: ",dictionary["Phone Number"])
+        #     elif user1__ == "all":
+        #         for key, value in dictionary.items():
+        #             print (key, ":", value)
+    # if user__ == 3:
+    #     a = input("are you sure? (y/n)")
+    #     if a == "y":
+    #         break
 
 
 
