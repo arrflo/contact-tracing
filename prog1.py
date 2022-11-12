3d862171326e64219d06c271e3cc5bf3d57d73bd
romalynfloranda <romalynfloranda0203@gmail.com>
2022-11-12 14:31:58 +0800

    changed dictionary

--- prog1.py
+++ prog1.py
@@ -4,19 +4,19 @@
 #2 - search 
 #3 - exit (y/n)
 
-#printmenu
+#print menu
 print ("------ Menu ------\n \n 1 - add an item \n 2 - search \n 3 - exit (y/n) \n")
 print ("------------------")
 print ()
 #ask user what to do
 user__ = int(input ("What would you like to do? "))
-#create a dictionary that stores info
-dict1 = {
-    "Fullname" : input ("Fullname: "),
-    "Age" : input ("Age: "),
-    "Address" : input("Address: "),
-    "Email" : input ("Email: "),
-    "Phone Number" : input ("Phone Number: ")
+#empty dictionary
+dictionary = {
+    "Fullname" : "  ",
+    "Age" : "  ",
+    "Address" : "  ",
+    "Email" : "  ",
+    "Phone Number" : "  "
 }
-#
+
 
