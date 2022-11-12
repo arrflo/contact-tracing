e6a02e09121bd42a190e461abe3a35e9a0ab9bfd
romalynfloranda <romalynfloranda0203@gmail.com>
2022-11-12 12:47:04 +0800

    added dictionary

--- prog1.py
+++ prog1.py
@@ -1,11 +1,22 @@
 #write a python program for contact tracing
 #display menu of options
-#1 - add an item (aadd ng info?)
-#2 - search (hahanapin yung ininput sa option 1?)
+#1 - add an item 
+#2 - search 
 #3 - exit (y/n)
 
 #printmenu
 print ("------ Menu ------\n \n 1 - add an item \n 2 - search \n 3 - exit (y/n) \n")
 print ("------------------")
+print ()
+#ask user what to do
+user__ = int(input ("What would you like to do? "))
 #create a dictionary that stores info
+dict1 = {
+    "Fullname" : input ("Fullname: "),
+    "Age" : input ("Age: "),
+    "Address" : input("Address: "),
+    "Email" : input ("Email: "),
+    "Phone Number" : input ("Phone Number: ")
+}
 #
+
