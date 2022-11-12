66c3ad91b4d94795934aec8e7a8debe94cdb72ae
romalynfloranda <romalynfloranda0203@gmail.com>
2022-11-12 12:01:49 +0800

    Menu is printed

---
+++ prog1.py
@@ -0,0 +1,11 @@
+#write a python program for contact tracing
+#display menu of options
+#1 - add an item (aadd ng info?)
+#2 - search (hahanapin yung ininput sa option 1?)
+#3 - exit (y/n)
+
+#printmenu
+print ("------ Menu ------\n \n 1 - add an item \n 2 - search \n 3 - exit (y/n) \n")
+print ("------------------")
+#create a dictionary that stores info
+#
